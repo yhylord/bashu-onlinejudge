7390ed1e98e5eed5326691e6fd5b525ad753d61e
Zhang <593141477@qq.com>
2012-11-08 15:29:07 +0800

    Update gcs

--- web/code/index.php
+++ web/code/index.php
@@ -23,28 +23,12 @@ $index_text= ($row=mysql_fetch_row($res)) ? $row[0] : '';
     <link href="../assets/css/bootstrap-responsive.css" rel="stylesheet">
     <link href="../assets/css/docs.css" rel="stylesheet">
 
-    <!-- Put the following javascript before the closing </head> tag. 
-    <script>
-      (function() {
-        var cx = '009214133664915278822:jxtzwpnxnz0';
-        var gcse = document.createElement('script'); gcse.type = 'text/javascript'; gcse.async = true;
-        gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') +
-            '//www.google.com/cse/cse.js?cx=' + cx;
-        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(gcse, s);
-      })();
-    </script>-->
     <!--[if IE 6]>
     <link href="ie6.min.css" rel="stylesheet">
     <![endif]-->
     <!--[if lt IE 9]>
       <script src="../assets/js/html5.js"></script>
     <![endif]-->
-    <script type="text/javascript" async>
-    (function(opts_){window.__gcse=window.__gcse||{};window.__gcse.ct=(new Date).getTime();window.__gcse.sacb=function(){};window.__gcse.scb=function(){var a=window.__gcse;a.plainStyle&&delete opts_.rawCss;google.search.cse.element.init(opts_)&&("explicit"!=a.parsetags?"complete"==document.readyState||"interactive"==document.readyState?(google.search.cse.element.go(),a.callback&&a.callback()):google.setOnLoadCallback(function(){google.search.cse.element.go();a.callback&&a.callback()},!0):a.callback&&a.callback())};
-var b=document.createElement("script"),c=opts_.protocol+"://"+opts_.uds+"/jsapi?autoload=",d=encodeURIComponent,e='{"name":"search","version":"1.0","callback":"__gcse.scb"',f=window.__gcse;if(!f||!f.plainStyle){var g=opts_.protocol+"://www.google.com/cse/style/look/",h;h=opts_.theme.toLowerCase().replace("v2_","v2/");e+=',"style":"'+(g+h+".css")+'"'}opts_.language&&(e+=',"language":"'+opts_.language+'"');e+="}";
-b.src=c+d('{"modules":['+e+',{"name":"ads","version":"3","packages":["search"],"callback":"__gcse.sacb"}]}')+"";b.type="text/javascript";document.getElementsByTagName("head")[0].appendChild(b);
-})({"cx":"009214133664915278822:jxtzwpnxnz0","language":"zh-Hans","theme":"V2_DEFAULT","uiOptions":{"resultsUrl":"","enableAutoComplete":false,"enableImageSearch":true,"imageSearchLayout":"popup","resultSetSize":"filtered_cse","enableOrderBy":false,"orderByOptions":[]},"protocol":"https","uds":"www.google.com","rawCss":"\n"});
-    </script>
   </head>
 
   <body>
@@ -112,7 +96,22 @@ b.src=c+d('{"modules":['+e+',{"name":"ads","version":"3","packages":["search"],"
       <div class="row-fluid">
         <div class="span10 offset1 well">
           <h4 class="center">Google Search</h4>
-          <gcse:search></gcse:search>
+          <div id="cse" style="width: 100%;">Loading</div>
+          <script src="https://www.google.com/jsapi" type="text/javascript"></script>
+          <script type="text/javascript"> 
+            google.load('search', '1', {language : 'zh-CN', style : google.loader.themes.V2_DEFAULT});
+            google.setOnLoadCallback(function() {
+              var customSearchOptions = {};
+              var imageSearchOptions = {};
+              imageSearchOptions['layout'] = google.search.ImageSearch.LAYOUT_POPUP;
+              customSearchOptions['enableImageSearch'] = true;
+              customSearchOptions['imageSearchOptions'] = imageSearchOptions;  var customSearchControl = new google.search.CustomSearchControl(
+                '009214133664915278822:jxtzwpnxnz0', customSearchOptions);
+              customSearchControl.setResultSetSize(google.search.Search.FILTERED_CSE_RESULTSET);
+              customSearchControl.draw('cse');
+            }, true);
+          </script>
+
         </div>
       </div>
       <hr>
