11e346684eb73d2b049ed98916181ece644944c4
yhylord <yhylord@gmail.com>
2016-02-10 11:11:49 +0800

    Initial version of creating contests

---
+++ web/code/editcontest.php
@@ -0,0 +1,124 @@
+<?php
+if (!isset($_SESSION['uesr'], $_SESSION['administrator'])) {
+  die('You are not the administrator');
+}
+
+/**
+ * Class ContestEditor
+ *
+ * Creates or edits contests.
+ *
+ * Receives title, start time (required), end time (required), description, id (if edit) and problems (required) of contests
+ * , checks them (throws exceptions if needed) and saves.
+ */
+class ContestEditor
+{
+  private $title, $start_time, $end_time, $description, $id, $problems;
+
+  public function __construct()
+  {
+    require 'inc/database.php';
+
+    $this->title = isset($_POST['title']) ? mysql_real_escape_string($_POST['title']) : '';
+
+    if (!isset($_POST['start_time'])) {
+      throw new Exception('Start time not set');
+    }
+    $this->start_time = mysql_real_escape_string($_POST['start_time']);
+    //TODO add min value
+
+    if (!isset($_POST['end_time'])) {
+      throw new Exception('End time not set');
+    }
+    $this->end_time = mysql_real_escape_string($_POST['end_time']);
+    //TODO add min value
+
+    $this->description = isset($_POST['description']) ? mysql_real_escape_string($_POST['description']) : '';
+
+    if (!isset($_POST['problems'])) {
+      throw new Exception('Problems not set');
+    }
+    /**
+     * Split input by line break
+     *
+     * @see http://stackoverflow.com/a/11165332/3226641
+     */
+    $this->problems = preg_split('/\n|\r/', $_POST['problems'], -1, PREG_SPLIT_NO_EMPTY);
+    if (!is_array($this->problems)) {
+      throw new Exception('Invalid problems');
+    }
+    $problem_id_max = mysql_fetch_row(mysql_query("SELECT MAX(problem_id) FROM problem"))[0];
+    $problem_id_min = mysql_fetch_row(mysql_query("SELECT MIN(problem_id) FROM problem"))[0];
+    foreach ($this->problems as $index => &$problem) {
+      if (!is_numeric($problem) || !($problem_id_min <= intval($problem) && intval($problem) <= $problem_id_max)) {
+        throw new Exception("Invalid problem ID: $problem (No. $index in the problem list)");
+      }
+      $problem = intval($problem);
+    }
+  }
+
+  public function add()
+  {
+    $this->id = 1;
+    $result = mysql_query("SELECT MAX(contest_id) FROM contest");
+    if (($row = mssql_fetch_row($result)) && intval($row[0])) {
+      $this->id = intval($row[0]) + 1;
+    }
+    $add_contest_info = "INSERT INTO contest (contest_id, title, start_time, end_time, description)
+                VALUES ($this->id, '$this->title', '$this->start_time', '$this->end_time', '$this->description')";
+    $this->test($add_contest_info);
+    $this->add_problems();
+    header("Location: contestpage.php?contest_id=$this->id");
+  }
+
+  /**
+   * Executes MySQL statements and throws exceptions if failed.
+   *
+   * @param $query string a MySQL statement to be executed
+   * @throws Exception if query failed
+   */
+  private function test($query)
+  {
+    if (!mysql_query($query)) {
+      throw new Exception("Operation failed");
+    }
+  }
+
+  private function add_problems()
+  {
+    foreach ($this->problems as $problem) {
+      $title = mysql_fetch_row(mysql_query("SELECT title FROM problem WHERE problem_id=$problem"))[0];
+      $add_problem = "INSERT INTO contest_problem (problem_id, contest_id, title) VALUES ($problem, $this->id, '$title')";
+      $this->test($add_problem);
+    }
+  }
+
+  public function edit()
+  {
+    if (!isset($_POST['contest_id'])) {
+      throw new Exception('No such contest');
+    }
+    $this->id = intval($_POST['contest_id']);
+    $update_contest_info = "UPDATE contest
+              SET title='$this->title', start_time='$this->start_time', end_time='$this->end_time', description='$this->title'
+              WHERE contest_id=$this->id";
+    $this->test($update_contest_info);
+    $delete_previous_problems = "DELETE FROM contest_problem WHERE contest_id=$this->id";
+    $this->test($delete_previous_problems);
+    $this->add_problems();
+    header("Location: contestpage.php?contest_id=$this->id");
+  }
+}
+
+if (!isset($_POST['op'])) {
+  echo 'Invalid operation.';
+}
+
+try {
+  $editor = new ContestEditor();
+  $editor->$_POST['op']();
+} catch (Exception $e) {
+  echo $e->getMessage();
+  return;
+}
+echo 'success';
---
+++ web/code/newcontest.php
@@ -0,0 +1,133 @@
+<?php
+require 'inc/checklogin.php';
+if (!isset($_SESSION['user'], $_SESSION['administrator'])) {
+  $info = 'You are not administrator';
+}
+$Title = "New contest";
+?>
+<!DOCTYPE html>
+<html>
+  <?php require 'head.php'; ?>
+  <body>
+    <?php require 'page_header.php' ?>
+    <!-- TODO Align with newproblem.php -->
+    <div class="container-fluid edit-page">
+      <?php
+      if (isset($info)) {
+        die($info);
+      }
+      ?>
+      <form action="editcontest.php" method="post" id="edit_form" style="padding-top: 10px">
+        <input type="hidden" name="op" value="add" required>
+        <div class="row-fluid">
+          <div class="span9">
+            <p><span>Title: </span><textarea title="Title" class="span8" name="title" id="contest-title"
+                                             rows="1"></textarea></p>
+          </div>
+        </div>
+        <div class="row-fluid">
+          <div class="span9">
+            <p><span>Start Time: <input title="Start Time" type="datetime-local" name="start_time"
+                                        id="contest-start-time" required></span></p>
+          </div>
+        </div>
+        <div class="row-fluid">
+          <div class="span9">
+            <p><span>End Time: <input title="End Time" type="datetime-local" name="end_time" id="contest-end-time"
+                                      required></span></p>
+          </div>
+        </div>
+        <div class="row-fluid">
+          <div class="span9">
+            <p>
+              <span>Description:</span>
+              <textarea title="Description" class="span12" name="description" id="contest-description"
+                        rows="13"></textarea>
+            </p>
+          </div>
+        </div>
+        <div class="row-fluid">
+          <div class="span9">
+            <p>
+              <span>Problems:</span>
+              <textarea title="Problems" name="problems" class="span12" id="" cols="30" rows="10"></textarea>
+            </p>
+          </div>
+        </div>
+        <div class="row-fluid">
+          <div class="span9" style="text-align:center">
+            <input type="submit" class="btn btn-primary" value="Submit">
+          </div>
+        </div>
+      </form>
+    </div>
+    <div class="html-tools">
+      <div class="well well-small margin-0" id="tools">
+        <table class="table table-bordered table-condensed table-striped" style="width:100%">
+          <caption>HTML Tools</caption>
+          <thead>
+            <tr>
+              <th>Function</th>
+              <th>Description</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>
+                <button class="btn btn-mini" id="tool_less">Less(&lt;)</button>
+              </td>
+              <td>&amp;lt;</td>
+            </tr>
+            <tr>
+              <td>
+                <button class="btn btn-mini" id="tool_greater">Greater(&gt;)</button>
+              </td>
+              <td>&amp;gt;</td>
+            </tr>
+            <tr>
+              <td>
+                <button class="btn btn-mini" id="tool_img">Image</button>
+              </td>
+              <td>&lt;img src=&quot;...&quot;&gt;</td>
+            </tr>
+            <tr>
+              <td>
+                <button class="btn btn-mini" id="tool_sup">Superscript</button>
+              </td>
+              <td>&lt;sup&gt;...&lt;/sup&gt;</td>
+            </tr>
+            <tr>
+              <td>
+                <button class="btn btn-mini" id="tool_sub">Subscript</button>
+              </td>
+              <td>&lt;sub&gt;...&lt;/sub&gt;</td>
+            </tr>
+            <tr>
+              <td>
+                <button class="btn btn-mini" id="tool_samp">Monospace</button>
+              </td>
+              <td>&lt;samp&gt;...&lt;/samp&gt;</td>
+            </tr>
+            <tr>
+              <td>
+                <button class="btn btn-mini" id="tool_inline">Inline TeX</button>
+              </td>
+              <td>[inline]...[/inline]</td>
+            </tr>
+            <tr>
+              <td>
+                <button class="btn btn-mini" id="tool_tex">TeX</button>
+              </td>
+              <td>[tex]...[/tex]</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <div style="text-align:center;margin-top:10px">
+        <button class="btn btn-info" id="btn_upload">Upload Image</button>
+      </div>
+    </div>
+    <!--TODO Add datetimepicker -->
+    <!--TODO Client side validation -->
+  </body>
+</html>
